90841e6304f2b9925f2483aee2573dbaf1d4b16f
Jonnatan Oyarzun <jonsxaero@gmail.com>
2019-02-15 16:28:46 -0300

    Saves user and skill to database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,25 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const bodyParser = require("body-parser").urlencoded({ extended: false });
 
-exports.addSkill = functions.https.onRequest((req, res) => {
-  if (req.method === "POST") {
-    return res.status(200).send("success");
-  }
-  return res.status(500).end();
-});
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.addSkill = functions.https.onRequest((req, res) =>
+  bodyParser(req, res, () => {
+    if (req.method === "POST") {
+      const username = req.body.user_name;
+      const text = req.body.text;
+
+      return db
+        .collection("users")
+        .doc(username)
+        .set({ skill: text })
+        .then(snapshot =>
+          res.status(200).send(`Successful added skill ${snapshot.skill}`)
+        );
+    }
+
+    return res.status(500).end();
+  })
+);
--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,12 +1,38 @@
-// const admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const test = require("firebase-functions-test")();
+jest.mock("firebase-admin");
 
 describe("index", () => {
   describe("addSkill", () => {
-    let myFunctions;
+    let myFunctions, setFn, docFn, req, res;
 
     beforeEach(() => {
+      admin.initializeApp.mockReturnValue();
+
+      setFn = jest.fn().mockResolvedValue({ skill: "skill" });
+      docFn = jest.fn().mockReturnValue({ set: setFn });
+
+      const db = {
+        collection: () => ({
+          doc: docFn
+        })
+      };
+
+      admin.firestore = jest.fn().mockReturnValue(db);
+
       myFunctions = require("../index");
+
+      req = { body: { user_name: "jona", text: "skill" } };
+      req.method = "POST";
+      req.headers = {};
+      req.headers["transfer-encoding"] = "utf-8";
+
+      res = {};
+      res.statusCode = 500;
+      res.status = statusCode => {
+        res.statusCode = statusCode;
+        return res;
+      };
     });
 
     afterEach(() => {
@@ -14,17 +40,19 @@ describe("index", () => {
     });
 
     it("responds 200 only on POST verb", done => {
-      const req = {};
-      req.method = "POST";
-
-      const res = {};
-      res.statusCode = 500;
-      res.status = statusCode => {
-        res.statusCode = statusCode;
-        return res;
-      };
       res.send = () => {
         expect(res.statusCode).toBe(200);
+        done();
+      };
+
+      myFunctions.addSkill(req, res);
+    });
+
+    it("adds user and skill to database", done => {
+      res.send = message => {
+        expect(message).toEqual("Successful added skill skill");
+        expect(docFn).toBeCalledWith("jona");
+        expect(setFn).toBeCalledWith({ skill: "skill" });
         done();
       };
 
