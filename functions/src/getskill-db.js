750a8124f6bee45ad079bb828f9eaa1c60dc7b3d
Jonnatan Oyarzun <jonsxaero@gmail.com>
2019-02-27 14:01:42 -0300

    Add log when is requesting a specific user

--- functions/src/getskill-db.js
+++ functions/src/getskill-db.js
@@ -27,7 +27,10 @@ const init = () => {
 
     let required_user_id = user_id;
     const userReg = /<@([WU][0-9A-Z]+)\|?\S*>/;
-    if (text && text.match(userReg)) required_user_id = text.match(userReg)[1];
+    if (text && text.match(userReg)) {
+      required_user_id = text.match(userReg)[1];
+      console.log(`Requesting a specific user: ${required_user_id}`);
+    }
 
     return getSkillByUser(required_user_id, team_id).then(doc => {
       const list = Object.keys(doc.data().skills).map(v => `• ${v}\n`);
