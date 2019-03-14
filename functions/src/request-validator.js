7b702e62ca039ffb94a110456e8e7260ec9e9041
Jonnatan Oyarzun <jonsxaero@gmail.com>
2019-03-14 01:41:03 -0300

    Add log to slack signature

--- functions/src/request-validator.js
+++ functions/src/request-validator.js
@@ -11,11 +11,15 @@ const getSignature = req => {
     "v0:" + timestamp + ":" + querystring.stringify(req.body);
   hmac.update(sig_basestring);
   const my_signature = "v0=" + hmac.digest("hex");
+  console.log("my_signature", my_signature);
+
   return Buffer.from(my_signature);
 };
 
 const validator = req => {
   const slack_signature = Buffer.from(req.headers["x-slack-signature"]);
+  console.log("slack_signature", req.headers["x-slack-signature"]);
+
   const my_signature = getSignature(req);
   return crypto.timingSafeEqual(slack_signature, my_signature);
 };
